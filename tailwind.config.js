128df29a883287f31e4b431907e898afdd1cf08b
commonbrushtail <53653641+commonbrushtail@users.noreply.github.com>
2021-07-01 00:23:54 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  purge: {
+    content: ["./dist/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      xs: "375px",
+      // => @media (min-width: 375px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "769px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      yellow: "#fdb913",
+      green: "#009444",
+      "dark-green": "#002a28",
+      white: "#fffbf3",
+      black: "#002a28",
+    },
+    fontFamily: {
+      robato: ["Roboto"],
+      prompt: ["Prompt"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
